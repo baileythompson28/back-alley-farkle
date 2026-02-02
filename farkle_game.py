c73a00181e0dc5c3cf9f49d92297f45fcae50633
CMP_BaThompson <CMP_BaThompson>
2026-02-02 08:58:25 -0500

    other players turn and winner

--- farkle_game.py
+++ farkle_game.py
@@ -3,24 +3,24 @@ def ask_user():
     num_players = int(input("How many players are playing? "))
     points_to_play_to = int(input("How many points are you playing to? "))
     print(f"Starting game with {num_players} players, Playing till {points_to_play_to} points.")
+    main()
 
 
 def main():
     """starts the players turn"""
-    ask_user()
     players_turn = input("Enter player's name for their turn: ")
-    round_points = 0
     print(f"{players_turn}'s turn")
-    print(f"Current points for the round: {round_points}")
+    dice_roll()
 
 
 def dice_roll():
     """rolls 3 dice and displays result"""
-    from dice_roller_class import DiceRoller
-    dice_roller = DiceRoller(3)
+    import dice_roller_class
+    num_of_dice = 3
+    dice_roller = dice_roller_class.DiceRoller(num_of_dice)
     dice_roller.roll()
     dice_roller.print_dice()
-    return dice_roller.values
+    print(f"total: {dice_roller.get_total()}")
 
 
 def determine_points(roll_values):
@@ -35,12 +35,27 @@ def determine_points(roll_values):
     points += counts[1] * 100
     points += counts[5] * 50
     if points == 0:
-        print("Farkle.")
+        print("Farkle. You get no points this round.")
         return 0
     print(f"Points scored: {points}")
     return points
 
+def next_turn():
+    """next player's turn"""
+    next_player = input("Enter next player's name for their turn: ")
+    print(f"{next_player}'s turn")
+    dice_roll()
+
+def winner():
+    """checks for winner"""
+    winner = "Player 1"
+    print(f"The winner is {winner}!")
+    play_again = input("Play again? (yes/no): ")
+    if play_again.lower() == 'yes':
+        ask_user()
+    else:
+        print("bye bye")
 
 
 if __name__ == "__main__":
-    main()
+    ask_user()
